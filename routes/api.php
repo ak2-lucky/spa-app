8753a73f11a6e08be5c5507a652d3caff49d8631
Akira Enomoto <ak2monotoe@gmail.com>
2020-07-19 21:20:28 +0900

    タスク詳細API実装

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 
 Route::get('/tasks', 'TaskController@index');
+Route::get('/tasks/{task}', 'TaskController@show');
 });
